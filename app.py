0ceb8260201d225d52bff254c07f3c320bf9041f
guillemm0 <gmaluenda0@uoc.edu>
2026-03-27 16:12:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Visualització de Dades", layout="wide")
 
 pagines = st.navigation([
-    st.Page("pagines/espelmes.py",          title="Gràfic d'Espelmes"),
     st.Page("pagines/barres_apilades.py", title="Barres Apilades"),
+    st.Page("pagines/espelmes.py",          title="Gràfic d'Espelmes"),
     st.Page("pagines/marimekko.py",      title="Marimekko"),
 ])
 pagines.run()
